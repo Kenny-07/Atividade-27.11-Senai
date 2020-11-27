b6ff1ccdc8351806fae55c5535e2e6ed5bfa69d8
DEV <dev@info.local>
2020-11-27 13:25:15 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Atividade_27._11_Senai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
